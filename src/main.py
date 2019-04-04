Correcting hidden bug in values computation ...

src/main.py
@@ -4,10 +4,11 @@ import gym.wrappers
 from env_tools.wrapper import PreprocessWrapperPytorch
 
 from config import load_config
-from rl_agent.dqn_agent import DQNAgent, FullRandomAgent
+from rl_agent.dqn_agent import DQNAgent
 
 import tensorboardX
 import numpy as np
+import torch
 
 parser = argparse.ArgumentParser('Log Parser arguments!')
 
@@ -32,13 +33,7 @@ full_config, expe_path = load_config(env_config_file=args.env_config,
 writer = tensorboardX.SummaryWriter(expe_path)
 log_every = 100
 
-if "minigrid" in full_config["env_name"]:
-    game = PreprocessWrapperPytorch(gym.make(full_config["env_name"]))
-else:
-    game = gym.make(full_config["env_name"])
-
-#game = gym.wrappers.Monitor(game, directory=expe_path, force=True)
-
+game = gym.make(full_config["env_name"])
 
 episodes = full_config["stop"]["episodes_total"]
 score_success = full_config["stop"]["episode_reward_mean"]
@@ -57,12 +52,12 @@ if model_type == "dqn" :
                      n_action=game.action_space,
                      state_dim=game.observation_space,
                      discount_factor=discount_factor)
-elif model_type == "random" :
-    model = FullRandomAgent(config=full_config, n_action=game.action_space, state_dim=game.observation_space)
+else:
+    raise NotImplementedError("{} not available for model".format(full_config["agent_type"]))
 
 
 for num_episode in range(1, episodes):
-    state = game.reset()
+    state = torch.FloatTensor(game.reset()).unsqueeze(0)
     #game.render()
     done = False
     iter_this_ep = 0
@@ -71,14 +66,17 @@ for num_episode in range(1, episodes):
 
     while not done:
 
-        action = model.forward(state)
-        next_state, reward, done, info = game.step(action=action)
+        action = model.select_action(state)
+        next_state, reward, done, info = game.step(action=action.item())
+
+        reward = torch.FloatTensor([reward])
+        next_state = torch.FloatTensor(next_state).unsqueeze(0)
 
         #game.render()
         if done:
             next_state = None
 
-        model.push(state, action, next_state, reward)
+        model.push(state.to('cpu'), action, next_state, reward)
         model.optimize()
 
         state = next_state
@@ -92,19 +90,25 @@ for num_episode in range(1, episodes):
         if done:
             model.callback(epoch=num_episode)
 
-            reward_undiscount_list.append(reward_total_not_discounted)
-            reward_discount_list.append(reward_total_discounted)
+            reward_undiscount_list.append(reward_total_not_discounted.item())
+            reward_discount_list.append(reward_total_discounted.item())
 
             # todo create logging
-            if num_episode%100 == 0:
-                print("Reward at the end of ep #{}, n_timesteps {}, discounted rew : {} undiscounted : {}, current_eps {}".format(
-                    num_episode, total_iter, reward_total_discounted, reward_total_not_discounted, model.current_eps))
+            if num_episode%20 == 0:
 
-                writer.add_scalar("data/sum_reward_discounted", np.mean(reward_discount_list), total_iter)
-                writer.add_scalar("data/sum_reward_not_discounted", np.mean(reward_undiscount_list), total_iter)
+                reward_discount_mean = np.mean(reward_discount_list)
+                reward_undiscount_mean = np.mean(reward_undiscount_list)
+
+                print("Reward at the end of ep #{}, n_timesteps {}, discounted rew : {} undiscounted : {}, current_eps {}".format(
+                    num_episode, total_iter, reward_discount_mean, reward_undiscount_mean, model.current_eps))
+
+                writer.add_scalar("data/sum_reward_discounted", reward_discount_mean, total_iter)
+                writer.add_scalar("data/sum_reward_not_discounted", reward_undiscount_mean, total_iter)
                 writer.add_scalar("data/iter_per_ep", iter_this_ep, total_iter)
                 writer.add_scalar("data/epsilon", model.current_eps, total_iter)
                 writer.add_scalar("data/model_update", model.num_update, total_iter)
+                writer.add_scalar("data/model_update_ep", model.num_update, num_episode)
+
 
             reward_undiscount_list = []
             reward_discount_list = []

src/rl_agent/agent_utils.py
@@ -6,145 +6,26 @@ import torch.nn as nn
 import random
 import logging
 
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
-class Flatten(nn.Module):
-  def forward(self, x):
-    N, C, H, W = x.size() # read in N, C, H, W
-    return x.view(N, -1)
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory(object):
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
         self.position = 0
 
-    def push(self, state, action, next_state, reward, error=0):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-
-        self.memory[self.position] = Transition(state, action, next_state, reward)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        batch_size = np.min([batch_size, len(self.memory)])
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
-
-class PrioritizedMemory(object):
-
-    def __init__(self, capacity, alpha=0.6):
-        self.capacity = capacity
-
-        self.memory = []
-        self.errors = []
-        self.indexes = []
-
-        self.position = 0
-        self.alpha = alpha
-
-    def push(self, state, action, next_state, reward, error=0):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-
-        self.memory[self.position] = Transition(state, action, next_state, reward)
-        self.errors[self.position] = np.max(self.errors)
-
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        batch_size = np.min([batch_size, len(self.memory)])
-
-        #indexes = np.random.(0, len(self), batch_size, p=)
-
-
-
-
-        return random.sample(self.memory, batch_size)
-
-    def update(self, indexes):
-        pass
-
-    def __len__(self):
-        return len(self.memory)
-
-
-class ReplayMemoryRecurrent(object):
-
-    def __init__(self, capacity, max_seq_length=8):
-
-        self.capacity = capacity
-        self.max_seq_length = max_seq_length
-        self.memory = [[]]
-        self.position = 0
-
     def push(self, *args):
-        """
-        Saves a transition in the replay buffer
-        We deal with sequences so : 1 case in the memory buffer => a sequence until episode's end.
-        """
-        # To avoid storing everything on gpu
-        args = [arg.cpu() for arg in args]
-        self.memory[self.position].append(Transition(*args))
-
-    def sample(self, batch_size):
-
-        # Select randomly a sequence length
-        min_seq_length = np.random.randint(4, self.max_seq_length)
-        max_batch_size = batch_size//min_seq_length
-
-        trajectories = []
-        selected = set()
-
-        check_inf_loop = 0
-
-        while len(trajectories) != max_batch_size:
-            check_inf_loop += 1
-            if check_inf_loop > 100:
-                break
-
-            id_mem = np.random.randint(len(self.memory))
-
-            # Don't sample the same trajectorie twice.
-            if id_mem in selected:
-                continue
-            selected.add(id_mem)
-            random_traj = self.memory[id_mem]
-
-            if len(random_traj) < min_seq_length:
-                continue
-            elif len(random_traj) == min_seq_length:
-                trajectories.append(random_traj)
-            else:
-                trajectories.append(random_traj[len(random_traj)-min_seq_length:])
-
-                # limit_begin_id = len(random_traj) - min_seq_length + 1 # to include sup
-                # random_begin = np.random.randint(0,limit_begin_id)
-                # random_traj_cut = random_traj[random_begin:random_begin+min_seq_length]
-                # trajectories.append(random_traj_cut)
-
-
-        # trajectories are : [[ step 1, step 2, step 3], [step1, step2, step3]] etc..
-        # we want : [[ step1, step1], [step2, step2]]
-        batch_size = len(trajectories)
-        return zip(*trajectories), batch_size
-
-    def end_of_ep(self):
-        """
-        A episode ends
-        """
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
         self.position = (self.position + 1) % self.capacity
 
-        if len(self.memory) < self.capacity:
-            self.memory.append([])
-        else:
-            self.memory[self.position] = []
-
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
 
     def __len__(self):
         return len(self.memory)
@@ -158,14 +39,17 @@ def freeze_as_np_dict(tensor_dict):
 
 def check_params_changed(dict1, dict2):
     "Takes two parameters dict (key:torchtensor) and prints a warning if identical"
+
+    dict1, dict2 = dict1, dict2
+
     for key in dict1.keys():
         if key.split('.')[-1] in ['running_mean', 'running_var']:
             # No message if this is for BatchNorm
             continue
         tmp1 = dict1[key]
         tmp2 = dict2[key]
-        if np.max(np.abs(tmp1 - tmp2))==0:
-            logging.debug('No change in params {}'.format(key))
+        if torch.max(torch.abs(tmp1 - tmp2)).item() == 0:
+            print('No change in params {}'.format(key))
 
 def compute_slow_params_update(slow_params, fast_params, tau):
 

src/rl_agent/dqn_agent.py
@@ -1,62 +1,43 @@
-import numpy as np
+from rl_agent.agent_utils import Transition, ReplayMemory, check_params_changed
 import torch
-import torch.optim as optim
 import torch.nn.functional as F
-from torch.autograd import Variable
-from copy import deepcopy
 
-from rl_agent.agent_utils import ReplayMemory, PrioritizedMemory, Transition, \
-    freeze_as_np_dict, compute_slow_params_update, ReplayMemoryRecurrent
+from rl_agent.models import FullyConnectedModel, ConvModel
+import numpy as np
+from copy import copy, deepcopy
 
 from rl_agent.gpu_utils import TORCH_DEVICE
 
-from rl_agent.models import FullyConnectedModel, ConvModel
-
-import logging
-from copy import copy
-
-
-class FullRandomAgent(object):
-    def __init__(self, config, n_action, state_dim):
-        self.n_action = n_action.n
-    def forward(self, *args, **kwargs):
-        return np.random.randint(0, self.n_action, size=None)
-    def optimize(self, *args, **kwargs):
-        pass
-    def callback(self, epoch):
-        pass
-
 class DQNAgent(object):
+
     def __init__(self, config, n_action, state_dim, discount_factor):
 
-        if config["model_type"] == "fc":
-            model = FullyConnectedModel(config=config["model_params"],
-                                        n_action=n_action,
-                                        state_dim=state_dim)
-        else:
-            model = ConvModel(config=config["model_params"],
-                                        n_action=n_action,
-                                        state_dim=state_dim)
-
-
-        self.fast_model = model.to(TORCH_DEVICE)
-        self.ref_model = deepcopy(self.fast_model).to(TORCH_DEVICE).eval()
-
+        self.discount_factor = discount_factor
         self.n_action = n_action.n
 
-        self.tau = config['tau']
+        self.lr = config["learning_rate"]
         self.batch_size = config["batch_size"]
-        self.soft_update = config["soft_update"]
 
-        self.clamp_grad = config["clamp_grad"]
+        self.memory = ReplayMemory(config["memory_size"])
 
-        if config["memory"] == "prioritized":
-            self.memory = PrioritizedMemory(config["memory_size"])
-        else:
-            self.memory = ReplayMemory(config["memory_size"])
+        self.update_every_n_ep = config["update_every_n_ep"]
+
+        self.num_update = 0
+
+        Model = FullyConnectedModel if config["model_type"] == "fc" else ConvModel
+
+        self.policy_net = Model(config["model_params"], n_action=n_action, state_dim=state_dim).to(TORCH_DEVICE)
+        self.target_net = Model(config["model_params"], n_action=n_action, state_dim=state_dim).to(TORCH_DEVICE)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.target_net.eval()
+
+
+        self.optimizer = torch.optim.RMSprop(self.policy_net.parameters(), lr=self.lr)
 
         if config["exploration_method"]["name"] == "eps_greedy":
-            self.forward = self._select_action_eps_greedy
+
+            self.select_action = self._select_action_eps_greedy
             self.epsilon_init = config["exploration_method"]["begin_eps"]
             self.current_eps = copy(self.epsilon_init)
             self.expected_exploration_steps = config["exploration_method"]["expected_step_explo"]
@@ -64,171 +45,85 @@ class DQNAgent(object):
             self.n_step_eps = 0
 
         else:
-            raise NotImplementedError("Wrong action selection method, chosen : {}".format(
-                config["exploration_method"]["name"]))
-
-        self.lr = config['learning_rate']
-        self.discount_factor = discount_factor
-
-        if config['optimizer'].lower() == 'rmsprop':
-            self.optimizer = optim.RMSprop(self.fast_model.parameters(), lr=self.lr)
-        elif config['optimizer'].lower() == 'adam':
-            self.optimizer = optim.Adam(self.fast_model.parameters(), lr=self.lr)
-        else:
-            assert False, 'Optimizer not recognized'
-
-        # logging.info('Model summary :')
-        # logging.info(self.forward_model.forward)
-
-        self.num_update = 0
-
-    def callback(self, epoch):
-        if not self.soft_update and epoch % int(1/self.tau) == 0:
-            self.ref_model.load_state_dict(self.fast_model.state_dict())
-            self.num_update += 1
-
-    def train(self):
-        self.fast_model.train()
-        self.ref_model.train()
-
-    def eval(self):
-        self.fast_model.eval()
-        self.ref_model.eval()
-
-    def format_state(self, state):
-
-        # state is {"env_state" : img, "objective": img/text}
-        var_state = dict()
-
-        env_state = state['image'] if isinstance(state, dict) else state
-
-        var_state['env_state'] = torch.FloatTensor(env_state).unsqueeze(0).to(TORCH_DEVICE)
-        return var_state
+            raise NotImplementedError("Boltzman explo not available ({})".format(config["exploration_method"]["name"]))
 
     def _select_action_eps_greedy(self, state):
-
-        if self.fast_model.train:
-            epsilon = self.minimum_epsilon
-        else:
-            epsilon = self.current_eps
-
-        random_number = np.random.rand()
-        if random_number < epsilon:
-            action = np.random.randint(self.n_action)
-        else:
-            var_state = self.format_state(state)
-            with torch.no_grad():
-                action = self.fast_model(var_state).data.max(1)[1].cpu().numpy()[0]
 
         # Formula for eps decay :
         # exp(- 2.5 * iter / expected iter)  2.5 is set by hand, just to have a smooth decay until end
         # init = 100%     end = 5%
         # 0% : eps=1      10% : eps=0.77    50% : eps=0.28     80% : eps=0.13
-        if self.fast_model.train :
-            self.current_eps = max(self.minimum_epsilon, self.epsilon_init * np.exp(- 2.5 * self.n_step_eps / self.expected_exploration_steps))
-            self.n_step_eps += 1
+        if self.policy_net.train:
+            self.current_eps = max(self.minimum_epsilon, self.epsilon_init * np.exp(
+                - 2.5 * self.n_step_eps / self.expected_exploration_steps))
 
-        return action
+        self.n_step_eps += 1
 
-    def push(self, state, action, next_state, reward):
-        # with torch.no_grad():
-        #
-        #     current_est = self.fast_model(self.format_state(state)).detach()[0][action] # [0] is for batch_dimension
-        #
-        #     if next_state != None:
-        #         next_state_val = self.ref_model(self.format_state(next_state)).detach().max()
-        #         target_val = reward + self.discount_factor * next_state_val
-        #     else:
-        #         target_val = reward
-        #
-        #     error = abs(current_est - target_val)
+        if np.random.random() > self.current_eps:
+            with torch.no_grad():
+                # t.max(1) will return largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                qs = self.policy_net(state.to(TORCH_DEVICE))
+                return qs.max(1)[1].view(1, 1).to('cpu')
+        else:
+            return torch.tensor([[np.random.randint(self.n_action)]], dtype=torch.long)
 
-        state = state['image'] if isinstance(state, dict) else state
-        next_state = next_state['image'] if isinstance(next_state, dict) else next_state
+    def push(self, *args):
+        self.memory.push(*args)
 
-        # state is {"env_state" : img, "objective": img}
-        state = torch.FloatTensor(np.expand_dims(state, axis=0))
+    def callback(self, epoch):
 
-        if isinstance(next_state, np.ndarray) :
-            next_state = torch.FloatTensor(np.expand_dims(next_state, axis=0))
-
-        action = torch.LongTensor(np.expand_dims(np.array([int(action)]), axis=0))
-        reward = torch.FloatTensor(np.array([reward]))
-
-        error = 0
-        self.memory.push(state, action, next_state, reward, error)
+        if epoch % self.update_every_n_ep == 0:
+            self.target_net.load_state_dict(self.policy_net.state_dict())
+            self.num_update += 1
 
     def optimize(self):
-        #  Optimize with respect to replay buffer
-        # =======================================
-
         if len(self.memory) < self.batch_size:
             return
-        else:
-            batch_size = self.batch_size
-
-        transitions = self.memory.sample(batch_size)
+        transitions = self.memory.sample(self.batch_size)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
 
-        state_batch = torch.cat(batch.state).to(TORCH_DEVICE)
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                              batch.next_state)), device=TORCH_DEVICE, dtype=torch.uint8)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None]).to(TORCH_DEVICE)
 
+        state_batch = torch.cat(batch.state).to(TORCH_DEVICE)
         action_batch = torch.cat(batch.action).to(TORCH_DEVICE)
         reward_batch = torch.cat(batch.reward).to(TORCH_DEVICE)
 
-        if len(state_batch.shape) == 3:
-            state_batch = state_batch.unsqueeze(0)
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
 
-        state_obj = dict()
-        state_obj['env_state'] = state_batch
-
-        state_action_values = self.fast_model(state_obj).gather(1, action_batch)
-
-        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None, batch.next_state))).to(TORCH_DEVICE)
-        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None]).to(TORCH_DEVICE)
-        # check if requires grad
-
-        non_final_next_states_obj = dict()
-        non_final_next_states_obj['env_state'] = non_final_next_states
-
-        if len(non_final_next_states.shape) == 3:
-            non_final_next_states = non_final_next_states.unsqueeze(0)
-
-        # Don't rembember gradients when computing the Q-value reference.
-        next_state_values = torch.zeros(batch_size).to(TORCH_DEVICE)
-        next_state_values[non_final_mask] = self.ref_model(non_final_next_states_obj).max(1)[0]
-        next_state_values.detach()
-
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size, device=TORCH_DEVICE)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
+        # Compute the expected Q values
         expected_state_action_values = (next_state_values * self.discount_factor) + reward_batch
 
-        loss_q_learning = F.smooth_l1_loss(state_action_values, expected_state_action_values)
-        loss = loss_q_learning
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 
-        #old_params = freeze_as_np_dict(self.forward_model.state_dict())
+        # Optimize the model
         self.optimizer.zero_grad()
         loss.backward()
 
-        if self.clamp_grad:
-            for name, param in self.fast_model.named_parameters():
-                #logging.debug(param.grad.data.sum())
-                param.grad.data.clamp_(-1., 1.)
+        #old_params = deepcopy(self.policy_net.state_dict())
 
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
 
-        # Update slowly ref model towards fast model, to stabilize training.
-        if self.soft_update:
-            self.ref_model.load_state_dict(compute_slow_params_update(self.ref_model, self.fast_model, self.tau))
-
-        # new_params = freeze_as_np_dict(self.fast_model.state_dict())
-        # check_params_changed(old_params, new_params)
-
-        return loss.detach().item()
-
-    def save_state(self):
-        # Store the whole agent state somewhere
-        state_dict = self.fast_model.state_dict()
-        memory = deepcopy(self.memory)
-        return state_dict, memory
-
-    def load_state(self, state_dict, memory):
-        self.fast_model.load_state_dict(state_dict)
-        self.memory = deepcopy(memory)
+        #check_params_changed(old_params, self.policy_net.state_dict())

src/rl_agent/models.py
@@ -13,32 +13,16 @@ class FullyConnectedModel(nn.Module):
         super().__init__()
 
         self.output_size = n_action.n
+        self.n_input = state_dim.shape[0]
 
-        self.n_hidden_mlp1 = config["n_mlp_hidden1"]
-        self.n_hidden_mlp2 = config["n_mlp_hidden2"]
+        self.n_hidden_mlp = config["n_mlp_hidden"]
 
-        if isinstance(state_dim, gym.spaces.Box):
-            self.n_input = np.product(state_dim.shape)
-        else: #  if "image" in state_dim.spaces["image"]:
-            self.n_input = np.product(state_dim.spaces["image"].shape)
-
-        self.additionnal_input_size = 0
-
-        self.hidden_layer1 = nn.Linear(self.n_input + self.additionnal_input_size, self.n_hidden_mlp1)
-        self.hidden_layer2 = nn.Linear(self.n_hidden_mlp1, self.n_hidden_mlp2)
-
-
-        self.out_layer = nn.Linear(self.n_hidden_mlp2, self.output_size)
+        self.hidden_layer = nn.Linear(self.n_input, self.n_hidden_mlp)
+        self.out_layer = nn.Linear(self.n_hidden_mlp, self.output_size)
 
     def forward(self, x):
 
-        x = x["env_state"]
-
-        batch_size = x.size(0)
-        x = x.view(batch_size, -1)
-
-        x = F.relu(self.hidden_layer1(x))
-        x = F.relu(self.hidden_layer2(x))
+        x = F.relu(self.hidden_layer(x))
         x = self.out_layer(x)
 
         return x

src/rl_agent/rllib_utils.py
@@ -6,8 +6,6 @@ from ray.rllib.models import ModelCatalog, Model
 import tensorflow as tf
 import sonnet as snt
 
-from rl_agent.model_utils import get_init_conv, get_init_mlp
-
 from env_tools.wrapper import ObsSpaceWrapper
 
 def env_basic_creator(env_name):
@@ -58,6 +56,19 @@ class ConvPolicy(Model):
 
         return fc, feat_stem
 
+
+
+def get_init_conv():
+    return {
+        "w": tf.contrib.layers.xavier_initializer_conv2d(),
+        "b": tf.truncated_normal_initializer(stddev=1.0)
+    }
+
+def get_init_mlp():
+    return {
+        "w": tf.contrib.layers.xavier_initializer(),
+        "b": tf.truncated_normal_initializer(stddev=1.0)
+    }
 
 
 ModelCatalog.register_custom_model("conv_policy", ConvPolicy)