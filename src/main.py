c7a0e5d41613763528489cc328e24263f86cfe8a
Seurin Mathieu <matseurin@cegetel.net>
2019-04-18 19:14:15 +0200

    Adding parallel running of expe, super easy using ray

--- src/main.py
+++ src/main.py
@@ -15,175 +15,186 @@ import torch
 import os
 import time
 
+import ray
 
 from env_tools.car_racing import CarRacingSafe
 
-parser = argparse.ArgumentParser('Log Parser arguments!')
+@ray.remote(num_gpus=0.5)
+def train(env_config, env_ext, model_config, model_ext, exp_dir, seed, local_test):
 
-parser.add_argument("-env_config",   type=str)
-parser.add_argument("-env_ext",      type=str)
-parser.add_argument("-model_config", type=str)
-parser.add_argument("-model_ext",    type=str)
-parser.add_argument("-exp_dir",      type=str, default="out", help="Directory all results")
-parser.add_argument("-seed",         type=int, default=42, help="Random seed used")
-parser.add_argument("-local_test",   type=bool, default=False, help="Random seed used")
+    print("Expe",env_config, env_ext, model_config, model_ext, exp_dir, seed, sep='  ')
+    print("Is cuda available ?", torch.cuda.is_available())
 
-
-args = parser.parse_args()
-DEFAULT_FRAME_SKIP = 3
+    if local_test:
+        display = open('nothing.txt', 'w')
+    else :
+        from xvfbwrapper import Xvfb
+        display = Xvfb(width=100, height=100, colordepth=16)
 
 
-if args.local_test:
-    display = open('nothing.txt', 'w')
-else :
-    from xvfbwrapper import Xvfb
-    display = Xvfb(width=100, height=100, colordepth=16)
+    full_config, expe_path = load_config(env_config_file=env_config,
+                              model_config_file=model_config,
+                              env_ext_file=env_ext,
+                              model_ext_file=model_ext,
+                              out_dir=exp_dir,
+                              seed=seed
+                              )
+
+    writer = tensorboardX.SummaryWriter(expe_path)
+    log_every = 100
 
 
-full_config, expe_path = load_config(env_config_file=args.env_config,
-                          model_config_file=args.model_config,
-                          env_ext_file=args.env_ext,
-                          model_ext_file=args.model_ext,
-                          out_dir=args.exp_dir,
-                          seed=args.seed
-                          )
+    if "safe" in full_config["env_name"].lower():
+        reset_when_out = full_config["reset_when_out"]
+        reward_when_out = full_config["reward_when_out"]
+        max_steps = full_config["max_steps"]
 
-writer = tensorboardX.SummaryWriter(expe_path)
-log_every = 100
+        game = CarRacingSafe(reset_when_out=reset_when_out,
+                             reward_when_out=reward_when_out,
+                             max_steps=max_steps)
 
+        DEFAULT_FRAME_SKIP = 3
+        n_frameskip = full_config.get("frameskip", DEFAULT_FRAME_SKIP)
 
-if "safe" in full_config["env_name"].lower():
-    reset_when_out = full_config["reset_when_out"]
-    reward_when_out = full_config["reward_when_out"]
-    max_steps = full_config["max_steps"]
+        game = CarActionWrapper(game)
+        game = FrameStackWrapper(game, n_frameskip=n_frameskip)
 
-    game = CarRacingSafe(reset_when_out=reset_when_out,
-                         reward_when_out=reward_when_out,
-                         max_steps=max_steps)
+    else:
+        game = gym.make(full_config["env_name"])
 
-    n_frameskip = full_config.get("frameskip", DEFAULT_FRAME_SKIP)
+    episodes = full_config["stop"]["episodes_total"]
+    score_success = full_config["stop"]["episode_reward_mean"]
 
-    game = CarActionWrapper(game)
-    game = FrameStackWrapper(game, n_frameskip=n_frameskip)
+    discount_factor = full_config["discount_factor"]
+    total_iter = 0
+    success_count = 0
 
-else:
-    game = gym.make(full_config["env_name"])
+    num_episode = 0
+    early_stopping = False
 
-episodes = full_config["stop"]["episodes_total"]
-score_success = full_config["stop"]["episode_reward_mean"]
+    reward_undiscount_list = []
+    reward_discount_list = []
 
-discount_factor = full_config["discount_factor"]
-total_iter = 0
-success_count = 0
+    best_undiscount_reward = -float("inf")
 
-num_episode = 0
-early_stopping = False
+    model_type = full_config["agent_type"]
+    if model_type == "dqn" :
+        model = DQNAgent(config=full_config["dqn_params"],
+                         n_action=game.action_space,
+                         state_dim=game.observation_space,
+                         discount_factor=discount_factor,
+                         biased_sampling=full_config["biased_sampling"])
+    else:
+        raise NotImplementedError("{} not available for model".format(full_config["agent_type"]))
 
-reward_undiscount_list = []
-reward_discount_list = []
+    with display as xvfb:
 
-best_undiscount_reward = -float("inf")
+        while num_episode < episodes and not early_stopping :
 
-model_type = full_config["agent_type"]
-if model_type == "dqn" :
-    model = DQNAgent(config=full_config["dqn_params"],
-                     n_action=game.action_space,
-                     state_dim=game.observation_space,
-                     discount_factor=discount_factor,
-                     biased_sampling=full_config["biased_sampling"])
-else:
-    raise NotImplementedError("{} not available for model".format(full_config["agent_type"]))
+            state = game.reset()
+            state['state'] = torch.FloatTensor(state['state']).unsqueeze(0)
+            state['gave_feedback'] = torch.FloatTensor([state['gave_feedback']])
 
-with display as xvfb:
+            #game.render('human')
+            done = False
+            iter_this_ep = 0
+            reward_total_discounted = 0
+            reward_total_not_discounted = 0
+            percentage_tile_seen = 0
+            n_feedback_this_ep = 0
 
-    while num_episode < episodes and not early_stopping :
+            while not done:
 
-        state = game.reset()
-        state['state'] = torch.FloatTensor(state['state']).unsqueeze(0)
-        state['gave_feedback'] = torch.FloatTensor([state['gave_feedback']])
+                action = model.select_action(state['state'])
+                next_state, reward, done, info = game.step(action=action.item())
 
-        #game.render('human')
-        done = False
-        iter_this_ep = 0
-        reward_total_discounted = 0
-        reward_total_not_discounted = 0
-        percentage_tile_seen = 0
-        n_feedback_this_ep = 0
+                reward = torch.FloatTensor([reward])
+                next_state['state'] = torch.FloatTensor(next_state['state']).unsqueeze(0)
+                next_state['gave_feedback'] = torch.FloatTensor([next_state['gave_feedback']])
 
-        while not done:
+                #game.render()
+                # if done:
+                #     next_state = None
 
-            action = model.select_action(state['state'])
-            next_state, reward, done, info = game.step(action=action.item())
+                model.push(state['state'].to('cpu'), action, next_state['state'], reward, next_state['gave_feedback'])
+                if not local_test:
+                    model.optimize()
 
-            reward = torch.FloatTensor([reward])
-            next_state['state'] = torch.FloatTensor(next_state['state']).unsqueeze(0)
-            next_state['gave_feedback'] = torch.FloatTensor([next_state['gave_feedback']])
+                # plt.imshow(game._unconvert(state['state'][0][0].numpy()))
+                # plt.show()
 
-            #game.render()
-            # if done:
-            #     next_state = None
+                state = next_state
 
-            model.push(state['state'].to('cpu'), action, next_state['state'], reward, next_state['gave_feedback'])
-            if not args.local_test:
-                model.optimize()
+                total_iter += 1
+                iter_this_ep = iter_this_ep + 1
 
-            # plt.imshow(game._unconvert(state['state'][0][0].numpy()))
-            # plt.show()
+                percentage_tile_seen = max(info['percentage_road_visited'], percentage_tile_seen)
+                n_feedback_this_ep += info['gave_feedback']
 
-            state = next_state
+                assert max(next_state['gave_feedback']) == info['gave_feedback'], "Problem, info should contain the same info as state"
 
-            total_iter += 1
-            iter_this_ep = iter_this_ep + 1
+                reward_total_discounted += reward * (discount_factor ** iter_this_ep)
+                reward_total_not_discounted += reward
 
-            percentage_tile_seen = max(info['percentage_road_visited'], percentage_tile_seen)
-            n_feedback_this_ep += info['gave_feedback']
+                # print("step",total_iter)
+                # print(time.time() - timer)
 
-            assert max(next_state['gave_feedback']) == info['gave_feedback'], "Problem, info should contain the same info as state"
+            # DONE GO HERE :
+            model.callback(epoch=num_episode)
 
-            reward_total_discounted += reward * (discount_factor ** iter_this_ep)
-            reward_total_not_discounted += reward
+            reward_undiscount_list.append(reward_total_not_discounted.item())
+            reward_discount_list.append(reward_total_discounted.item())
 
-            # print("step",total_iter)
-            # print(time.time() - timer)
+            if reward_total_discounted > score_success:
+                success_count += 1
+                if success_count > 5:
+                    early_stopping = True
+            else:
+                success_count = 0
 
-        # DONE GO HERE :
-        model.callback(epoch=num_episode)
+            if num_episode % 2 == 0 or early_stopping:
+                reward_discount_mean = np.mean(reward_discount_list)
+                reward_undiscount_mean = np.mean(reward_undiscount_list)
 
-        reward_undiscount_list.append(reward_total_not_discounted.item())
-        reward_discount_list.append(reward_total_discounted.item())
+                print(
+                    "Reward at the end of ep #{}, n_timesteps {}, discounted rew : {} undiscounted : {}, current_eps {}".format(
+                        num_episode, total_iter, reward_discount_mean, reward_undiscount_mean, model.current_eps))
 
-        if reward_total_discounted > score_success:
-            success_count += 1
-            if success_count > 5:
-                early_stopping = True
-        else:
-            success_count = 0
+                writer.add_scalar("data/percentage_tile_seen", percentage_tile_seen, total_iter)
+                writer.add_scalar("data/number_of feedback", n_feedback_this_ep, total_iter)
+                writer.add_scalar("data/sum_reward_discounted", reward_discount_mean, total_iter)
+                writer.add_scalar("data/sum_reward_not_discounted", reward_undiscount_mean, total_iter)
+                writer.add_scalar("data/iter_per_ep", iter_this_ep, total_iter)
+                writer.add_scalar("data/epsilon", model.current_eps, total_iter)
+                writer.add_scalar("data/model_update", model.num_update, total_iter)
+                writer.add_scalar("data/model_update_ep", model.num_update, num_episode)
 
-        if num_episode % 2 == 0 or early_stopping:
-            reward_discount_mean = np.mean(reward_discount_list)
-            reward_undiscount_mean = np.mean(reward_undiscount_list)
+                if reward_discount_mean > best_undiscount_reward :
+                    best_undiscount_reward = reward_discount_mean
+                    torch.save(model.policy_net.state_dict(), os.path.join(expe_path,"best_model.pth"))
 
-            print(
-                "Reward at the end of ep #{}, n_timesteps {}, discounted rew : {} undiscounted : {}, current_eps {}".format(
-                    num_episode, total_iter, reward_discount_mean, reward_undiscount_mean, model.current_eps))
-
-            writer.add_scalar("data/percentage_tile_seen", percentage_tile_seen, total_iter)
-            writer.add_scalar("data/number_of feedback", n_feedback_this_ep, total_iter)
-            writer.add_scalar("data/sum_reward_discounted", reward_discount_mean, total_iter)
-            writer.add_scalar("data/sum_reward_not_discounted", reward_undiscount_mean, total_iter)
-            writer.add_scalar("data/iter_per_ep", iter_this_ep, total_iter)
-            writer.add_scalar("data/epsilon", model.current_eps, total_iter)
-            writer.add_scalar("data/model_update", model.num_update, total_iter)
-            writer.add_scalar("data/model_update_ep", model.num_update, num_episode)
-
-            if reward_discount_mean > best_undiscount_reward :
-                best_undiscount_reward = reward_discount_mean
-                torch.save(model.policy_net.state_dict(), os.path.join(expe_path,"best_model.pth"))
-                
-            torch.save(model.policy_net.state_dict(), os.path.join(expe_path, "last_model.pth"))
+                torch.save(model.policy_net.state_dict(), os.path.join(expe_path, "last_model.pth"))
 
 
 
-        num_episode += 1
+            num_episode += 1
 
-    print("Experiment over")
+        print("Experiment over")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser('Log Parser arguments!')
+
+    parser.add_argument("-env_config", type=str)
+    parser.add_argument("-env_ext", type=str)
+    parser.add_argument("-model_config", type=str)
+    parser.add_argument("-model_ext", type=str)
+    parser.add_argument("-exp_dir", type=str, default="out", help="Directory all results")
+    parser.add_argument("-seed", type=int, default=42, help="Random seed used")
+    parser.add_argument("-local_test", type=bool, default=False, help="If env is run on my PC or a headless server")
+
+    args = parser.parse_args()
+
+    ray.init(num_gpus=1, local_mode=True)
+    ray.get(train.remote(args.env_config, args.env_ext, args.model_config, args.model_ext, args.exp_dir, args.seed, args.local_test))
---
+++ src/multiple_run.py
@@ -0,0 +1,52 @@
+from main import train
+import argparse
+
+import json
+import ray
+
+import os
+
+@ray.remote(num_gpus=0.3)
+def dummy_train(a = 10):
+    import torch
+    print(a)
+    print(torch.cuda.is_available())
+
+@ray.remote
+def dummy_train2(a = 10):
+    import torch
+    print(a)
+    print(torch.cuda.is_available())
+
+
+def read_multiple_config_file(config_path):
+
+    json_config = json.load(open(os.path.join("config/multiple_run_config", config_path), "r"))
+
+    all_expe_to_run = []
+    for ext in json_config["model_ext"]:
+        expe_config = {}
+        expe_config["env_config"] = json_config["common"]["env_config"]
+        expe_config["env_ext"] = ''
+        expe_config["model_config"] = json_config["common"]["model_config"]
+        expe_config["seed"] = json_config["common"]["seed"]
+
+        expe_config["model_ext"] = ext
+
+        all_expe_to_run.append(expe_config)
+
+    return all_expe_to_run
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser('Log Parser arguments!')
+
+    parser.add_argument("-multiple_run_config", type=str)
+    parser.add_argument("-n_gpus", type=int, default=4)
+
+    args = parser.parse_args()
+
+    configs = read_multiple_config_file(args.multiple_run_config)
+
+    ray.init(num_gpus=2)
+    ray.get([train.remote(**config) for config in configs])
